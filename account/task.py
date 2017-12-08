d2cf1ff0c031b67fb7f97864db71f01e5ffe5da2
gagan <gagandeeptrivedi47@gmail.com>
2017-12-08 12:46:46 +0530

    importing os in task.py now

--- account/task.py
+++ account/task.py
@@ -1,8 +1,10 @@
+import os
 from django.core.mail import send_mail
 
 from celery import shared_task
 
+
 @shared_task
-def send_welcome_mail(username,email_id):
-    send_mail('Welcome', 'welcome' + username , os.environ['USER_EMAIL'], [email_id])
+def send_welcome_mail(username, email_id):
+    send_mail('Welcome', 'welcome' + username, os.environ['USER_EMAIL'], [email_id])
     return True
